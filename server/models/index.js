6cea03ec0ca7c0e639b300be8b36c3086526309d
Eric Geniesse <egeniesse@gmail.com>
2015-12-19 16:26:47 -0800

    Continuing to refactor the code.

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,37 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+    	var sql = "select messages.id, messages.text from messages \
+    								left outer join on (messages.room_id = rooms.id) \
+    								left outer join on (messages.user_id = users.id \
+    								order by messages.id desc";
+    	db.query(sql, function(err, result){
+    		callback(result);
+    	});
+    }, 
+    post: function (params, callback) {
+    	var sql = "insert into messages (id, text, room_id, user_id) \
+    					   values (?,?,?,?)";
+    	db.query(sql, params, function(err, result){
+    		callback(result);
+    	});
+    } 
   },
 
   users: {
-    // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+    	var sql = "select * from users";
+    	db.query(sql, function(err, result){
+    		callback(result);
+    	});
+    },
+    post: function (params, callback) {
+    	var sql = "insert into users (username) values (?)";
+    	db.query(sql, params, function(err, result){
+    		callback(result);
+    	});
+    }
   }
 };
 
--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `text` VARCHAR(150) NULL DEFAULT NULL,
+  `username` VARCHAR(30) NULL DEFAULT NULL,
+  `user_id` INTEGER NULL DEFAULT NULL,
+  `roomname` VARCHAR(30) NULL DEFAULT NULL,
+  `room_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `roomname` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `messages` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (room_id) REFERENCES `rooms` (`id`);
 
 /* Create other tables and define schemas for them here! */
 
